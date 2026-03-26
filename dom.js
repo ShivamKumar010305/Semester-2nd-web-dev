99528f114f453c2c1905b280afe1accf9a8d9ee6
ShivamKumar010305 <shivam.25b01010305@abes.ac.in>
2026-03-26 22:43:01 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,24 @@
+//document.writeln("Hello");
+function result(){
+ // let inputData=document.getElementById("inputText").value;
+  //document.getElementById('text').textContent=inputData;
+
+    let changeColor=document.getElementById('text');
+    changeColor.style.color="red";
+
+    let bodyColor=document.getElementsByTagName('body')[0];
+    bodyColor.style.backgroundColor="green";
+}
+
+//let para=document.getElementById("paratext");
+//function changetextColor(){
+    //para.style.backgroundColor="blue";
+//}
+//para.addEventListener('mouseover',changetextColor);
+//let para = document.getElementById("paratext");
+
+//function changetextColor(){ 
+   //para.style.backgroundColor = "blue"; 
+//}
+
+//para.addEventListener('mouseover', changetextColor);
